4ca6263b0d1dddace7a7c3a21bd587c132495a03
‮sgniD ot duP <dhyslexia@gmail.com>
2026-04-14 22:54:35 +0700

    add gh workflows

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,3 +15,9 @@
 		<a href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}>{locale}</a>
 	{/each}
 </div>
+
+// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
+// pages by setting this here, but should prerender as many as possible to
+// avoid large performance and SEO impacts
+export const prerender = true;
+export const trailingSlash = 'always';
